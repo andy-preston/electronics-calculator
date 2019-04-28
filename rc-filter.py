04d470692d6039c086a3fc9cf810491e3698a80f
Andy Preston <edgeeffect@gmail.com>
2019-04-28 11:13:07 +0100

    added electrolytic capacitors to list

--- rc-filter.py
+++ rc-filter.py
@@ -39,8 +39,8 @@ resistors = [
     '2M2', '2M4', '2M7', '3M3', '3M9', '4M7', '5M6', '6M8', '8M2', '10M'
 ]
 
-# www.bitsbox.co.uk Ceramic Disc Capacitors
 capacitors = [
+    # www.bitsbox.co.uk Ceramic Disc Capacitors
     '1pF', '2.2pF', '2.7pF', '3.3pF', '4.7pF', '5pF', '6.8pF',
     '10pF', '15pF', '18pF',
     '22pF', '25pF', '27pF', '33pF', '47pF', '50pF', '68pF',
@@ -50,9 +50,16 @@ capacitors = [
     '2200pF', '2700pF', '3300pF', '3900pF', '4700pF', '6800pF',
     '10nF', '15nF',
     '22nF', '33nF', '47nF', '100nF', '220nF',
+    # www.bitsbox.co.uk 85deg Electrolytic Capacitors (skipping voltage ratings)
+    '0.1uF', '0.22uF', '0.47uF', '1uF', '2.2uF', '3.3uF', '4.7uF',
+    '10uF', '22uF', '33uF', '47uF',
+    '100uF', '220uF', '330uF', '470uF',
+    '1000uF', '2200uF', '3300uF', '4700uF', '6800uF'
 ]
 
+electros = capacitorValue('0.1uF')
 pi2 = 2 * math.pi
+
 lp1Freq = 75.0
 lp1Resistor = ''
 lp1Capacitor = ''
@@ -72,7 +79,7 @@ for resistor in resistors:
             # a coupling capacitor / high pass filter. So we need a value which
             # will cut off below the 25Hz but, again, as close to 25Hz as we
             # can get.
-            if frequency > 25.0 and frequency < 75.0 and frequency < lp1Freq:
+            if C < electros and frequency > 25.0 and frequency < 75.0 and frequency < lp1Freq:
                 lp1Freq = frequency
                 lp1Resistor = resistor
                 lp1Capacitor = capacitor
